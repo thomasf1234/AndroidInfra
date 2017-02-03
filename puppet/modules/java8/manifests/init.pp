199166560bf071487b0e500fa02121e0e7822066
Thomas F <thomas.fisher@footballpools.com>
2017-02-03 18:04:54 +0000

    [#1,7] TF : updating the java8 manifest for JAVA_HOME environment variable

--- puppet/modules/java8/manifests/init.pp
+++ puppet/modules/java8/manifests/init.pp
@@ -3,7 +3,7 @@ class java8 {
     command => "/usr/bin/apt-get update"
   }
 
-  file {'java-seeds':
+  file {'license-seeds':
     path => "/tmp/oracle-java-license.seeds",
     source => 'puppet:///modules/java8/oracle-java-license.seeds',
     ensure => 'present',
@@ -13,12 +13,12 @@ class java8 {
   package {'oracle-java8-installer':
     ensure => installed,
     responsefile => "/tmp/oracle-java-license.seeds",
-    require => File['java-seeds']
+    require => File['license-seeds']
   }
 
-  exec { 'foo':
-    environment => ["FOO=bar"],
-    command => '/bin/echo $FOO > /tmp/bar'
-    require =>
+  exec { 'update environment variables':
+    environment => ["JAVA_HOME=/usr/lib/jvm/java-8-oracle"],
+    command => '/bin/echo $FOO > /tmp/bar',
+    require => Package['oracle-java8-installer']
   }
 }
